667d5fe97fa7f6c119e26985d9ad6683710f90fd
Qodirov1808 <mhtorjonq@gmail.com>
2024-05-13 15:17:49 +0500

    update site

--- resources/views/dashboart/inc/header.blade.php
+++ resources/views/dashboart/inc/header.blade.php
@@ -9,11 +9,11 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 		<!--end::Fonts-->
 		<!--begin::Page Vendors Styles(used by this page)-->
-		<link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+		<link href="/dashboart/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
 		<!--end::Page Vendors Styles-->
 		<!--begin::Global Theme Styles(used by all pages)-->
 		<link href="{{asset('')}}dashboart/assets/plugins/global/plugins.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
-		<link href="{{asset('')}}dashboart/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+		<link href="/dashboart/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
 		<link href="{{asset('')}}dashboart/assets/css/style.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
